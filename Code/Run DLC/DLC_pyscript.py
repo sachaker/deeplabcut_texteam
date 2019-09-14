e617899d6755ddc9fd17df6c4ce55cc01cf54ce9
sacha.mcelligott <midgetman69@gmail.com>
2019-09-14 18:30:09 +0100

    Made some updates

--- Code/Run DLC/DLC_pyscript.py
+++ Code/Run DLC/DLC_pyscript.py
@@ -1,3 +1,7 @@
+# 
+# move this file to \Anaconda3\\envs\\dlc-windowsGPU\\Lib'
+
+
 # Modifications made by SM on 10.09.19
 
 # todo list (things to automate)
@@ -5,11 +9,11 @@
 	# set numframes2pick variable in .yaml file depending on num_subsessions --- Done! (SM, 10.09.19)
 	# find out how to create the shortcut programmatically to fix the symbolic link hack --- Done! (SM, 10.09.19)
 
-
-# make sure you open Spyder within the DLC environment from the Anaconda navigator
-
-
-# import deeplabcut - this takes a little while
+# to avoid running code when module is imported
+def main():
+    pass
+	
+###### Main ######
 import deeplabcut, os
 from datetime import datetime
 
@@ -23,45 +27,34 @@ session_path_mouse = 'Z:\\data\\HS\\DLC\\' + mouse
 session_path = 'Z:\\data\\HS\\DLC\\' + mouse + '\\' + date
 
 # make directory if it does not exist yet
-if not os.path.exists(session_path):
-    print('folder does not exist')
-    os.mkdir(session_path)
-    print('created folder')
 if not os.path.exists(session_path_mouse):
     print('folder does not exist')
     os.mkdir(session_path_mouse)
-    print('created folder')
+    print('created folder for mouse')
+if not os.path.exists(session_path):
+    print('folder does not exist')
+    os.mkdir(session_path)
+    print('created folder for session')
     
 # navigate to the data directory
 os.chdir(session_path)
 
-# os.getcwd() # to find the current path
-#os.mkdir("Z:\\data\\HS\\DLC\\cb135\\20170821")
-
 # video paths that are part of the subsessions to be labelled
-# modified code from: https://www.mkyong.com/python/python-how-to-list-all-files-in-a-directory/
 videoFiles = []
 count = 0;
 videoPath = 'Z:\\data\\HS\\avi\\' + mouse + '\\' + date
 for r, d, f in os.walk(videoPath):
     for file in f:
-	    if (count == num_subsessions+1):
-	        break
+        if (count == num_subsessions+1):
+            break
         if '.avi' in file:
-		    videoFiles.append(os.path.join(r, file))
-		count+=1
+            videoFiles.append(os.path.join(r, file))
+        count+=1
 list.sort(videoFiles)
 list.sort(f)
-#f = f[0:(num_subsessions-1)] # get rid of thumbs.db if it exists
 
-#create Project if it has not been created yet
 model_path = os.listdir(session_path)
-##if model_path:
-    # just create the path to the config file
-##    config_path = session_path+"\\"+model_path[0]+"\\config.yaml"
-##else: # create a new project with folders
     
-    ## NEED TO CHANGE IN CASE PROJECT FILE IS ALREADY THERE
 now = datetime.now()
 year = now.strftime("%Y")
 month = now.strftime("%m")
@@ -69,26 +62,19 @@ day = now.strftime("%d")
 	
 labeler = input('What are the initials of the primary labeler?: ')
 
-#Z:\data\HS\DLC\cb107\20170523\cb107-20170523-SM-2019-09-11
 projectPath = session_path + '\\' + mouse + '-' + date + '-' + labeler + '-' + year + '-' + month + '-' + day 
-
-#if not os.path.exists(projectPath):
-#    print('project folder does not exist')
-#    os.mkdir(projectPath)
-#    os.mkdir(projectPath + '\\videos')
-#    print('created project folder')
-	
-	
-totalFrames = 200
-numframes2pick = (totalFrames-(totalFrames%num_subsessions))/num_subsessions
-print('Number of frames to extract per video is ' + str(numframes2pick))
 
 ###create symlinks	
 ##for i in range(num_subsessions):
 	##dst = projectPath + '\\videos\\' + f[i]
     ##os.symlink(videoFiles[i], dst)
+	
+totalFrames = 200
+numframes = (totalFrames - (totalFrames % num_subsessions)) / num_subsessions
+	
+	
+config_path = deeplabcut.create_new_project(mouse + "-" + date, labeler, videoFiles) # just to be safe
 
-#####config_path = deeplabcut.create_new_project(mouse + "-" + date, labeler, videoFiles[0]) # just to be safe
 #####for i in range(num_subsessions):
 #####    deeplabcut.add_new_videos(config_path, [videoFiles[i]]) # should create synlinks for each
 
@@ -119,5 +105,7 @@ print('Number of frames to extract per video is ' + str(numframes2pick))
 
 # when you are done - save out pictures of the labeled frames
 #####deeplabcut.check_labels(config_path)
+
+
 
 
